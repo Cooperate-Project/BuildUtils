6564be370e640cdc675aac916c0254de2fad77b2
Stephan Seifermann <seifermann@fzi.de>
2016-11-01 10:12:17 +0100

    Also deploy tagged builds.

--- travisci/mavenBuild.sh
+++ travisci/mavenBuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-	echo mvn clean deploy sonar:sonar
-	mvn clean deploy sonar:sonar
-elif [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-	echo mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify sonar:sonar
-	mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify sonar:sonar
-else
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo mvn clean verify
 	mvn clean verify
+elif [ "$TRAVIS_BRANCH" = "master" ] || [ -n "$TRAVIS_TAG" ]; then
+	echo mvn clean deploy sonar:sonar
+	mvn clean deploy sonar:sonar
+else
+	echo mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify sonar:sonar
+	mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify sonar:sonar
 fi
