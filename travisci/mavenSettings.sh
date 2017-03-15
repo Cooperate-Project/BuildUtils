b86144c1f81d1dee14033b61498f190537376a98
Stephan Seifermann <seifermann@fzi.de>
2017-03-15 14:01:14 +0100

    Increased maven heap space to 1GB.

--- travisci/mavenSettings.sh
+++ travisci/mavenSettings.sh
@@ -5,3 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/../settings/settings.xml "$HOME/.m2/"
 cp $DIR/../settings/settings-security.xml "$HOME/.m2/"
 echo "<settingsSecurity><master>"$MVN_MASTERPW"</master></settingsSecurity>" > /tmp/settings-security.xml
+export MAVEN_OPTS="-Xmx1G -Xms128m"
