54f27ff78dede5f47ff988cc63f7c55e6bb50050
Stephan Seifermann <seifermann@fzi.de>
2017-03-20 20:46:10 +0100

    Speed up for sonar analysis.

--- travisci/mavenBuild.sh
+++ travisci/mavenBuild.sh
@@ -5,9 +5,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo mvn clean verify
 	mvn clean verify
 elif [ "$TRAVIS_BRANCH" = "master" ] || [ -n "$TRAVIS_TAG" ]; then
-	echo mvn clean deploy \&\& mvn sonar:sonar
-	mvn clean deploy && mvn sonar:sonar
+	echo mvn clean deploy \&\& mvn -Dtycho.mode=maven sonar:sonar
+	mvn clean deploy && mvn -Dtycho.mode=maven sonar:sonar
 else
-	echo mvn clean verify \&\& mvn -Dsonar.branch="$TRAVIS_BRANCH" sonar:sonar
-	mvn clean verify && mvn -Dsonar.branch="$TRAVIS_BRANCH" sonar:sonar
+	echo mvn clean verify \&\& mvn -Dsonar.branch="$TRAVIS_BRANCH" -Dtycho.mode=maven sonar:sonar
+	mvn clean verify && mvn -Dsonar.branch="$TRAVIS_BRANCH" -Dtycho.mode=maven sonar:sonar
 fi
