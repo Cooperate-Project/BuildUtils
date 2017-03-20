dd6821d4f451ac8fc62a1261e79b3a332af97b47
Stephan Seifermann <seifermann@fzi.de>
2017-03-20 08:50:38 +0100

    Possible workaround for sonar heap space issue

--- travisci/mavenBuild.sh
+++ travisci/mavenBuild.sh
@@ -5,9 +5,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo mvn clean verify
 	mvn clean verify
 elif [ "$TRAVIS_BRANCH" = "master" ] || [ -n "$TRAVIS_TAG" ]; then
-	echo mvn clean deploy sonar:sonar
-	mvn clean deploy sonar:sonar
+	echo mvn clean deploy && mvn sonar:sonar
+	mvn clean deploy && mvn sonar:sonar
 else
-	echo mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify sonar:sonar
-	mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify sonar:sonar
+	echo mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify && mvn sonar:sonar
+	mvn -Dsonar.branch="$TRAVIS_BRANCH" clean verify && mvn sonar:sonar
 fi
