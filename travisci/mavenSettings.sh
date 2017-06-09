758f9ea7881ff672ee4d687691553bb69861fb63
Stephan Seifermann <seifermann@fzi.de>
2017-06-09 14:46:33 +0200

    Adjusted memory consumption for maven.

--- travisci/mavenSettings.sh
+++ travisci/mavenSettings.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/../settings/settings.xml "$HOME/.m2/"
 cp $DIR/../settings/settings-security.xml "$HOME/.m2/"
 echo "<settingsSecurity><master>"$MVN_MASTERPW"</master></settingsSecurity>" > /tmp/settings-security.xml
-echo "MAVEN_OPTS='-Xmx3g -XX:MaxPermSize=512m'" > ~/.mavenrc
+echo "MAVEN_OPTS='-Xmx1792m'" > ~/.mavenrc
