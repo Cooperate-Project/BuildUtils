cf8c26e3031829748a9f0eb9d3f9d4b7b2ca7921
Stephan Seifermann <seifermann@fzi.de>
2017-10-05 08:05:22 +0200

    Allow defining branch to deploy.

--- travisci/mavenBuild.sh
+++ travisci/mavenBuild.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ -z ${MAIN_BRANCH+x} ]; then export MAIN_BRANCH=master; fi
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo mvn clean verify
 	mvn clean verify
-elif [ "$TRAVIS_BRANCH" = "master" ] || [ -n "$TRAVIS_TAG" ]; then
+elif [ "$TRAVIS_BRANCH" = "$MAIN_BRANCH" ] || [ -n "$TRAVIS_TAG" ]; then
 	echo clean deploy sonar:sonar
 	mvn clean deploy sonar:sonar
 else
--- travisci/mavenBuildWoSonar.sh
+++ travisci/mavenBuildWoSonar.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ "$TRAVIS_BRANCH" = "master" ] || [ -n "$TRAVIS_TAG" ]; then
+if [ -z ${MAIN_BRANCH+x} ]; then export MAIN_BRANCH=master; fi
+
+if [ "$TRAVIS_BRANCH" = "$MAIN_BRANCH" ] || [ -n "$TRAVIS_TAG" ]; then
 	echo clean deploy
 	mvn clean deploy
 else
