f05faf28447184b192039d62af61e9aac22f0fe5
Stephan Seifermann <seifermann@fzi.de>
2018-04-08 15:44:01 +0200

    Replaced key with base64 version.

--- travisci/signingSetup.sh
+++ travisci/signingSetup.sh
@@ -4,7 +4,7 @@ if [ -z ${MVN_MASTERPW+x} ]; then
 	echo "Skipping GPG initialization"
 else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	openssl aes-256-cbc -k $MVN_MASTERPW -in $DIR/cooperate.key.enc -out /tmp/cooperate.key -d
+	openssl enc -aes-256-cbc -a -d -in $DIR/cooperate.key.enc -out /tmp/cooperate.key -k $MVN_MASTERPW
 	echo $MVN_MASTERPW | gpg --passphrase-fd 0 --batch --yes --import /tmp/cooperate.key
 	rm /tmp/cooperate.key
 fi
