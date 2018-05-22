4aa84869bcd87e8366ebbd21e646efcf1127dd92
Stephan Seifermann <seifermann@fzi.de>
2018-05-22 16:31:24 +0200

    Disable sonar analysis because services is gone.

--- travisci/mavenBuild.sh
+++ travisci/mavenBuild.sh
@@ -7,9 +7,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo mvn clean verify
 	mvn clean verify
 elif [ "$TRAVIS_BRANCH" = "$MAIN_BRANCH" ] || [ -n "$TRAVIS_TAG" ]; then
-	echo clean deploy sonar:sonar
-	mvn clean deploy sonar:sonar
+	echo clean deploy
+	mvn clean deploy
 else
-	echo mvn clean verify sonar:sonar -Dsonar.branch="$TRAVIS_BRANCH"
-	mvn clean verify sonar:sonar -Dsonar.branch="$TRAVIS_BRANCH"
+	echo mvn clean verify
+	mvn clean verify
 fi
